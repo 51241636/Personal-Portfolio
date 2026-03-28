cde722a1fd1cde30cd005ab0e9ffa5f992d96dec
51241636 <pisepala@gmail.com>
2026-03-28 21:36:24 +0530

    add exersice section content

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const  section=document.querySelector(".exercise-section")
+const observer=new IntersectionObserver((entries)=>{
+  if(entries[0].isIntersecting){
+    entries[0].target.classList.add("show")
+  }else{
+    entries[0].target.classList.remove("show")
+  }
+}, {threshold : 0.5})
+observer.observe(section);
