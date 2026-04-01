762015b076ee6bd5394ebe744efab6e4fa656a64
51241636 <pisepala@gmail.com>
2026-04-01 20:17:51 +0530

    scrolling animation add

--- js/app.js
+++ js/app.js
@@ -7,3 +7,22 @@ const observer=new IntersectionObserver((entries)=>{
   }
 }, {threshold : 0.3})
 observer.observe(section);
+
+const  section02=document.querySelector("#aboutSection")
+const observer02=new IntersectionObserver((entries)=>{
+  if(entries[0].isIntersecting){
+    entries[0].target.classList.add("show")
+  }else{
+    entries[0].target.classList.remove("show")
+  }
+}, {threshold : 0.3})
+observer02.observe(section02);
+const  section03=document.querySelector(".technology-section")
+const observer03=new IntersectionObserver((entries)=>{
+  if(entries[0].isIntersecting){
+    entries[0].target.classList.add("show")
+  }else{
+    entries[0].target.classList.remove("show")
+  }
+}, {threshold : 0.3})
+observer03.observe(section03);
